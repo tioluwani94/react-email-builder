b4dd8258adcb27aaa7eb2645487225ba7c5730d9
Tioluwani Kolawole <kolawole8tiolu@gmail.com>
2023-09-10 18:32:39 +0100

    wip: button component editor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -86,7 +86,7 @@ export default function Home() {
                       backgroundColor: "#ffffff",
                     }}
                   >
-                    {state.tree?.map((block) => {
+                    {state.tree?.map((block: any) => {
                       return generateComponent(block);
                     })}
                   </Container>
